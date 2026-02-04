4b8c350016c5646977b3a0faa576312eb93bc939
DiegoBarreras <diegobarreras01@gmail.com>
2026-02-04 11:57:11 -0700

    Se editó de manera que el comando para la IP no dependa del nombre de las tarjetas de red.

--- tarea1_diagnostico.sh
+++ tarea1_diagnostico.sh
@@ -1,10 +1,8 @@
 echo -e "\e[0;35mPrimer Script en BASH\e[0m\n"
 echo "Nombre del Equipo:"
 hostname
-echo -e "\nIP de la tarjeta de red principal:"
-ip addr show enp0s3
-echo -e "\nIP de la tarjeta de red secundaria: (Red interna)"
-ip addr show enp0s8
+echo -e "\nIP de las tarjetas de red:"
+ip addr 
 echo -e "\nEspacio en disco del sistema:"
 df -h /
 
